e825adeb162a68733967cf738fdae69a42c1b405
Minatoto <thomas.gretouce@gmail.com>
2017-10-17 17:16:15 +0200

    Update DirectoryEvaluator.java

--- exercice2/src/service/DirectoryEvaluator.java
+++ exercice2/src/service/DirectoryEvaluator.java
@@ -9,10 +9,10 @@ public class DirectoryEvaluator {
         String list = "Arborescence : \n---------------\n";
         String[] paths;
 
-        // On récupère l'arborescence du dossier dans un tableau
+        // On récupère l'arborescence du dossier dans un tableau pour faciliter la récupération
         paths = directory.list();
 
-        // On lit le tableau et on le converti en string
+        // On lit le tableau et on le converti en string pour plus de confort pour la suite
         for(String path:paths) {
             list+=path + "\n";
         }
