766f1e78b2602c3aed5d252bbfc5a498b3c47e8c
Minatoto <thomas.gretouce@gmail.com>
2017-10-17 17:29:02 +0200

    Update PathEvaluator.java

--- exercice1/src/service/PathEvaluator.java
+++ exercice1/src/service/PathEvaluator.java
@@ -1,9 +1,8 @@
 package service;
-
 import java.io.File;
 
 public class PathEvaluator {
-
+    
     public void pathExist(String path){
 
         // Création d'un objet (file) correspondant au chemin
@@ -12,21 +11,23 @@ public class PathEvaluator {
         // On teste si le chemin existe bien ou pas 
         if(f.exists()) {
             System.out.println("Le chemin existe");
-        } else{
+        } 
+          else{
             System.out.println("Le chemin n'existe pas");
-        }
+          }
     }
-
+    
     public void pathType(String path){
 
         // Création d'un objet (file) correspondant au chemin
         File f = new File(path);
 
-        // On teste si l'objet est un dossier ou un fichier
-        if(f.isDirectory()) {
-            System.out.println("C'est un dossier");
-        } else if(f.isFile()) {
+        // On teste si l'objet est un fichier ou un dossier
+        if(f.isFile()) {
             System.out.println("C'est un fichier");
-        }
+        } 
+          else if(f.isDirectory()) {
+            System.out.println("C'est un dossier");
+          }
     }
 }
